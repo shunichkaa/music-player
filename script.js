739e4a84c4acb1f02988beafbdd9f05ac56600d0
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-28 16:05:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -82,5 +82,9 @@ const audio = new Audio();
 let userData = {
   songs: [...allSongs],
   currentSong: null,
-  songCurrentTime: 0
+  songCurrentTime: 0,
+};
+
+const printGreeting = () => {
+  console.log("Hello there!");
 };
