92aad775885623bb28c6fddc591b8066def12deb
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-28 15:58:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,3 @@ const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
-
-const allSongs = []
