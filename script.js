3d52aacc71acbf1092c31095ed9ed6328bd80b62
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-29 10:31:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -107,12 +107,17 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
-
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
       return -1;
     }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
   });
 };
 
