35982aca7064607fb586d48b6fd5e0ddcc352eb1
Aleksandra Mate <matealeksandra@gmail.com>
2025-06-30 11:28:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -92,6 +92,8 @@ const playSong = (id) => {
 
 if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
   audio.currentTime = 0;
+} else {
+  audio.currentTime = userData?.songCurrentTime;
 }
 };
 
