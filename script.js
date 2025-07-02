fb840a5d2780c5dd8c00b24aee2a8e63c3cf2db5
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-02 19:44:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
-
+  
   playButton.classList.remove("playing");
   audio.pause();
 };
@@ -121,15 +121,14 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-  if (userData?.currentSong === null) {
-    return;
-  } else {
+   if (userData?.currentSong === null) return;
+   else {
     const currentSongIndex = getCurrentSongIndex();
     const previousSong = userData?.songs[currentSongIndex - 1];
-    playSong(previousSong.id);
-  }
-};
 
+    playSong(previousSong.id);
+   }
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
@@ -166,6 +165,8 @@ playButton.addEventListener("click", () => {
 pauseButton.addEventListener("click",  pauseSong);
 
 nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
