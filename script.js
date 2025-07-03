5831e00834f940bc74454c2791d784195fc3820a
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-03 11:56:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -134,9 +134,9 @@ const highlightCurrentSong = () => {
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
-  
+
 playlistSongElements.forEach((songEl) => {
-  
+  songEl.removeAttribute("aria-current");
 });
 
 };
