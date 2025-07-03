34691e6780e99bbec127c29851788fa949c8b9c2
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-03 12:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -133,10 +133,14 @@ const playPreviousSong = () => {
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
-  
+
   const currentTitle = userData?.currentSong?.title;
   const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
+
 
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
