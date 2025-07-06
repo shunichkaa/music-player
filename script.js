c184bd383a03b9d3e90cdc0e32aa856f6209da18
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-06 12:06:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -205,9 +205,9 @@ const renderSongs = (array) => {
 
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
-  const resetText = document.createTextNode("Reset Playlist");
-
-  
+    const resetText = document.createTextNode("Reset Playlist");
+  resetButton.id = "reset";
+resetButton.ariaLabel = "Reset playlist";
   }
 
 };
