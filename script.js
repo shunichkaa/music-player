edd6bc95b007cde90df63f5be8077e310175717d
Aleksandra Mate <matealeksandra@gmail.com>
2025-07-06 11:59:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -143,10 +143,12 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 };
 
-const deleteSong = (id) => {
-if (userData?.currentSong?.id === id) {
+const deleteSong = (id) => {if (userData?.currentSong?.id === id) {
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  pauseSong();
+  setPlayerDisplay();
 }
-
   userData.songs = userData?.songs.filter((song) => song.id !== id);
   renderSongs(userData?.songs); 
   highlightCurrentSong(); 
